c1334da707e6eaa11022602df5313dd567353446
Mohab Ezzat <ezt.mohab10@gmail.com>
2024-03-24 01:32:21 +0200

    enhance question model with fetch model bug

--- app/src/main/java/mrandroid/app/activity/doctor/AddQuestionActivity.java
+++ app/src/main/java/mrandroid/app/activity/doctor/AddQuestionActivity.java
@@ -3,9 +3,15 @@ package mrandroid.app.activity.doctor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import mrandroid.app.R;
 import mrandroid.app.databinding.ActivityAddQuestionBinding;
 import mrandroid.app.model.AnswerModel;
@@ -17,10 +23,6 @@ public class AddQuestionActivity extends AppCompatActivity {
     private ActivityAddQuestionBinding binding;
     private boolean is4Options = true;
     private LoadingDialog loadingDialog;
-    private AnswerModel option1;
-    private AnswerModel option2;
-    private AnswerModel option3;
-    private AnswerModel option4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,40 +31,10 @@ public class AddQuestionActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         loadingDialog = new LoadingDialog(this);
-        option1 = new AnswerModel("", false);
-        option2 = new AnswerModel("", false);
-        option3 = new AnswerModel("", false);
-        option4 = new AnswerModel("", false);
 
         binding.rgOptionsNum.setOnCheckedChangeListener((radioGroup, id) -> {
             if (id == R.id.rbOptionNum2) setup2Options();
             else setup4Options();
-        });
-
-        binding.rgOptionsAnswer.setOnCheckedChangeListener((radioGroup, id) -> {
-            // reset
-            option1.setCorrect(false);
-            option2.setCorrect(false);
-            option3.setCorrect(false);
-            option4.setCorrect(false);
-
-            switch (id) {
-                case R.id.rbOption1:
-                    option1.setCorrect(true);
-                    break;
-
-                case R.id.rbOption2:
-                    option2.setCorrect(true);
-                    break;
-
-                case R.id.rbOption3:
-                    option3.setCorrect(true);
-                    break;
-
-                case R.id.rbOption4:
-                    option4.setCorrect(true);
-                    break;
-            }
         });
 
         binding.fabSubmit.setOnClickListener(view -> {
@@ -107,8 +79,9 @@ public class AddQuestionActivity extends AppCompatActivity {
 
         boolean hasCorrectAnswer;
         if (is4Options)
-            hasCorrectAnswer = option1.isCorrect() || option2.isCorrect() || option3.isCorrect() || option4.isCorrect();
-        else hasCorrectAnswer = option1.isCorrect() || option2.isCorrect();
+            hasCorrectAnswer = binding.rbOption1.isChecked() || binding.rbOption2.isChecked()
+                    || binding.rbOption3.isChecked() || binding.rbOption4.isChecked();
+        else hasCorrectAnswer = binding.rbOption1.isChecked() || binding.rbOption2.isChecked();
 
         if (!hasCorrectAnswer) {
             Toast.makeText(this, "Correct answer is required!", Toast.LENGTH_SHORT).show();
@@ -121,77 +94,22 @@ public class AddQuestionActivity extends AppCompatActivity {
     private void createQuestion() {
         loadingDialog.display();
 
-        String question = binding.etQuestion.getText().toString().trim();
         boolean isCode = binding.cbCode.isChecked();
-        QuestionModel questionModel = new QuestionModel(question,isCode);
+        String question = binding.etQuestion.getText().toString().trim();
+        String option1Text = binding.etOption1.getText().toString().trim();
+        String option2Text = binding.etOption2.getText().toString().trim();
+        String option3Text = binding.etOption3.getText().toString().trim();
+        String option4Text = binding.etOption4.getText().toString().trim();
+
+        List<AnswerModel> answerList = new ArrayList<>();
+        answerList.add(new AnswerModel(option1Text, binding.rbOption1.isChecked()));
+        answerList.add(new AnswerModel(option2Text, binding.rbOption2.isChecked()));
+        answerList.add(new AnswerModel(option3Text, binding.rbOption3.isChecked()));
+        answerList.add(new AnswerModel(option4Text, binding.rbOption4.isChecked()));
+        QuestionModel questionModel = new QuestionModel(question, is4Options,isCode, answerList);
 
         DatabaseReference questionRef = FirebaseDatabase.getInstance().getReference().child("questions").child(question);
         questionRef.setValue(questionModel)
-                .addOnSuccessListener(aVoid -> createAnswer1())
-                .addOnFailureListener(e -> {
-                    loadingDialog.dismiss();
-                    Toast.makeText(getBaseContext(), "Error adding answer: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
-
-    private void createAnswer1() {
-        loadingDialog.display();
-
-        String question = binding.etQuestion.getText().toString().trim();
-        String option1Text = binding.etOption1.getText().toString().trim();
-        option1.setAnswer(option1Text);
-
-        DatabaseReference answerRef = FirebaseDatabase.getInstance().getReference().child("questions").child(question).child("answer");
-        answerRef.push().setValue(option1)
-                .addOnSuccessListener(aVoid -> createAnswer2())
-                .addOnFailureListener(e -> {
-                    loadingDialog.dismiss();
-                    Toast.makeText(getBaseContext(), "Error adding answer: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
-
-    private void createAnswer2() {
-        String question = binding.etQuestion.getText().toString().trim();
-        String option2Text = binding.etOption2.getText().toString().trim();
-        option2.setAnswer(option2Text);
-
-        DatabaseReference answerRef = FirebaseDatabase.getInstance().getReference().child("questions").child(question).child("answer");
-        answerRef.push().setValue(option2)
-                .addOnSuccessListener(aVoid -> {
-                    if (is4Options) createAnswer3();
-                    else {
-                        loadingDialog.dismiss();
-                        Toast.makeText(getBaseContext(), "Question created successfully", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    loadingDialog.dismiss();
-                    Toast.makeText(getBaseContext(), "Error adding answer: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
-
-    private void createAnswer3() {
-        String question = binding.etQuestion.getText().toString().trim();
-        String option3Text = binding.etOption3.getText().toString().trim();
-        option3.setAnswer(option3Text);
-
-        DatabaseReference answerRef = FirebaseDatabase.getInstance().getReference().child("questions").child(question).child("answer");
-        answerRef.push().setValue(option3)
-                .addOnSuccessListener(aVoid -> createAnswer4())
-                .addOnFailureListener(e -> {
-                    loadingDialog.dismiss();
-                    Toast.makeText(getBaseContext(), "Error adding answer: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
-
-    private void createAnswer4() {
-        String question = binding.etQuestion.getText().toString().trim();
-        String option4Text = binding.etOption4.getText().toString().trim();
-        option4.setAnswer(option4Text);
-
-        DatabaseReference answerRef = FirebaseDatabase.getInstance().getReference().child("questions").child(question).child("answer");
-        answerRef.push().setValue(option4)
                 .addOnSuccessListener(aVoid -> {
                     loadingDialog.dismiss();
                     Toast.makeText(getBaseContext(), "Question created successfully", Toast.LENGTH_SHORT).show();
--- app/src/main/java/mrandroid/app/activity/student/StudentActivity.java
+++ app/src/main/java/mrandroid/app/activity/student/StudentActivity.java
@@ -1,18 +1,86 @@
 package mrandroid.app.activity.student;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import mrandroid.app.databinding.ActivityStudentBinding;
+import mrandroid.app.model.QuestionModel;
+import mrandroid.app.util.LoadingDialog;
 
 public class StudentActivity extends AppCompatActivity {
 
     private ActivityStudentBinding binding;
+    private LoadingDialog loadingDialog;
+    private List<QuestionModel> questionList = new ArrayList<>();
+    private int questionPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStudentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loadingDialog = new LoadingDialog(this);
+        getAllQuestions();
+    }
+
+    private void getAllQuestions() {
+        loadingDialog.display();
+        FirebaseDatabase.getInstance().getReference().child("questions").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                loadingDialog.dismiss();
+                for (DataSnapshot questionSnapshot : snapshot.getChildren()) {
+                    questionList.add(questionSnapshot.getValue(QuestionModel.class));
+                }
+                if (!questionList.isEmpty()) {
+                    binding.studentRoot.setVisibility(View.VISIBLE);
+                    questionPosition = 0;
+                    setCurrentQuestion(questionPosition);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                loadingDialog.dismiss();
+                Toast.makeText(getBaseContext(), "Error fetching questions: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setCurrentQuestion(int questionPosition) {
+        QuestionModel questionModel = questionList.get(questionPosition);
+
+        binding.etQuestion.setText(questionModel.getQuestion());
+        binding.etOption1.setText(questionModel.getAnswers().get(0).getAnswer());
+        binding.etOption2.setText(questionModel.getAnswers().get(1).getAnswer());
+        binding.etOption3.setText(questionModel.getAnswers().get(2).getAnswer());
+        binding.etOption4.setText(questionModel.getAnswers().get(3).getAnswer());
+
+        if (questionModel.isCode()) binding.tvOpenCode.setVisibility(View.VISIBLE);
+        else binding.tvOpenCode.setVisibility(View.GONE);
+
+        if (questionModel.isIs4Option()) {
+            binding.etOption3.setVisibility(View.VISIBLE);
+            binding.etOption4.setVisibility(View.VISIBLE);
+        }
+        else {
+            binding.etOption3.setVisibility(View.GONE);
+            binding.etOption4.setVisibility(View.GONE);
+        }
+
     }
 
 }
